3d65a5cfbd94309155a63c85796785166d8b43fa
chzhonge <chzhonge@gmail.com>
2020-07-04 15:43:58 +0800

    feat: ubuntu/install.sh 增加 pull docker image

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -6,10 +6,21 @@ declare -a packageList=(
  "docker.io docker-compose"
 )
 
+declare -a dockerImageList=(
+ "php:7.3-cli-alpine" "php:7.3.19-fpm-alpine"
+ "composer:2.0.0-alpha2" "memcached:1.6.6-alpine"
+ "nginx:1.19.0" "mysql:5.7.30" "phpmyadmin/phpmyadmin"
+)
+
 apt-get update
 
 # ref:https://stackoverflow.com/a/8880633
 for val in "${packageList[@]}"
 do
 	apt-get install ${val}
+done
+
+for val in "${dockerImageList[@]}"
+do
+        docker pull ${val}
 done
